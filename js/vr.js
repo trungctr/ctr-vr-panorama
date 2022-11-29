d90c23c2422a4c3b08d403e22bd5b3d1270ff505
trung_dev_be <trungdh@tradeline.vn>
2022-11-29 15:20:26 +0700

    init vr

--- js/vr.js
+++ js/vr.js
@@ -13,8 +13,135 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 /**
- * add GUI
+ * setup VR/XR
  */
+renderer.xr.enabled = true
+renderer.xr.setReferenceSpaceType('local')
+document.body.appendChild(VRButton.createButton(renderer))
+
+const controllerModelFactory = new XRControllerModelFactory()
+
+// controller
+const controller = renderer.xr.getController(0)
+scene.add(controller)
+
+const controllerGrip = renderer.xr.getControllerGrip(0)
+controllerGrip.add(controllerModelFactory.createControllerModel(controllerGrip))
+scene.add(controllerGrip)
+
+// controller
+const controller1 = renderer.xr.getController(1)
+scene.add(controller1)
+
+const controllerGrip1 = renderer.xr.getControllerGrip(1)
+controllerGrip1.add(
+	controllerModelFactory.createControllerModel(controllerGrip1)
+)
+scene.add(controllerGrip1)
+
+//
+const geometry = new THREE.BufferGeometry().setFromPoints([
+	new THREE.Vector3(0, 0, 0),
+	new THREE.Vector3(0, 0, -1)
+])
+
+const line = new THREE.Line(geometry)
+line.name = 'line'
+line.scale.z = 10
+
+controller.add(line.clone())
+controller1.add(line.clone())
+
+const selectPressed = false
+
+/**
+ * add GUI (deving)
+ */
+
+function onPrev() {
+	const msg = 'Prev pressed'
+	console.log(msg)
+	ui.updateElement('info', msg)
+}
+
+function onStop() {
+	const msg = 'Stop pressed'
+	console.log(msg)
+	ui.updateElement('info', msg)
+}
+
+function onNext() {
+	const msg = 'Next pressed'
+	console.log(msg)
+	ui.updateElement('info', msg)
+}
+
+function onContinue() {
+	const msg = 'Continue pressed'
+	console.log(msg)
+	ui.updateElement('info', msg)
+}
+
+const config = {
+	panelSize: { width: 2, height: 0.5 },
+	height: 128,
+	info: {
+		type: 'text',
+		position: { left: 6, top: 6 },
+		width: 500,
+		height: 58,
+		backgroundColor: '#aaa',
+		fontColor: '#000'
+	},
+	prev: {
+		type: 'button',
+		position: { top: 64, left: 0 },
+		width: 64,
+		fontColor: '#bb0',
+		hover: '#ff0',
+		onSelect: onPrev
+	},
+	stop: {
+		type: 'button',
+		position: { top: 64, left: 64 },
+		width: 64,
+		fontColor: '#bb0',
+		hover: '#ff0',
+		onSelect: onStop
+	},
+	next: {
+		type: 'button',
+		position: { top: 64, left: 128 },
+		width: 64,
+		fontColor: '#bb0',
+		hover: '#ff0',
+		onSelect: onNext
+	},
+	continue: {
+		type: 'button',
+		position: { top: 70, right: 10 },
+		width: 200,
+		height: 52,
+		fontColor: '#fff',
+		backgroundColor: '#2659a4',
+		hover: '#3df',
+		onSelect: onContinue
+	},
+	renderer: renderer
+}
+
+const content = {
+	info: '',
+	prev: '<path>M 10 32 L 54 10 L 54 54 Z</path>',
+	stop: '<path>M 50 15 L 15 15 L 15 50 L 50 50 Z<path>',
+	next: '<path>M 54 32 L 10 10 L 10 54 Z</path>',
+	continue: 'Continue'
+}
+
+let ui = new CanvasUI(content, config)
+
+ui.mesh.position.set(0, 1, -3)
+scene.add(ui.mesh)
 
 /**
  * add environment effects
@@ -42,9 +169,20 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 camera.position.set(0, 0, 0)
 camera.lookAt(0, 0, 1)
-document.onclick = () => {
+
+let pointerLocked = false
+document.addEventListener('DOMContentLoaded', () => {
 	pointerL.lock()
-}
+	pointerLocked = true
+})
+document.addEventListener('click', () => {
+	if (pointerLocked == false) {
+		pointerL.lock()
+	} else
+	{
+		pointerL.unlock()
+	}
+})
 
 /**
  * add xyz axis, x red, y green, z blue
@@ -99,6 +237,7 @@ maintainScene()
 
 function mainLoop() {
 	setUp()
+	ui.update()
 	renderer.render(scene, camera)
 }
 
